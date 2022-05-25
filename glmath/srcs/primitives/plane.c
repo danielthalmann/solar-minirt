5d23ff50e2094e7d5caf56e3d8b51c7fe72a37a2
Daniel Thalmann <dthalman@c1r5s8.42lausanne.ch>
2022-05-25 10:16:28 +0200

    return intersect of plane

--- glmath/srcs/primitives/plane.c
+++ glmath/srcs/primitives/plane.c
@@ -6,7 +6,7 @@
 /*   By: dthalman <daniel@thalmann.li>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 21:35:39 by dthalman          #+#    #+#             */
-/*   Updated: 2022/05/20 21:35:39 by dthalman         ###   ########.fr       */
+/*   Updated: 2022/05/25 10:12:59 by dthalman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,8 @@ int	plane_intersect(t_ray *ray, void *p, t_point3f *intersec)
 	if (denom > 1e-6) { 
 		p0l0 = v3f_minus(&plane->origin, &ray->origin);
 		t = v3f_scalar_product(&p0l0, &plane->normal) / denom;
-		v3f_copy(intersec, &ray->origin);
-		//v3f_copy(intersec, &p0l0);
+		v3f_copy(intersec, &plane->origin);
+		*intersec = ray_at(t, ray);
 		return (t >= 0.0); 
 	}
 	return (0);

afc6887e19a839589332835b47ac5b3a278a6868
Daniel Thalmann <daniel@thalmann.li>
2022-06-07 20:23:53 +0200

    update parser for norm

--- srcs/parsing/parse_cylinder.c
+++ srcs/parsing/parse_cylinder.c
@@ -51,7 +51,7 @@ int	parse_cylinder(t_scene *scene, char *str)
 	s->normal_ray = (void (*)(t_ray *, const void *))cylinder_normal_ray;
 	s->color_mask = (float (*)(const t_ray *, const void *))cylinder_color_mask;
 	s->color_normal = (t_color (*)(const t_ray *, const void *, float i))cylinder_color_normal;
-	s->color_texture = (t_color (*)(const t_ray *, const void *, t_image *i))cylinder_color_texture;
+	s->color_texture = (t_color (*)(const t_ray *, const void *, t_image * i))cylinder_color_texture;
 	n_parsed = ft_sscanf(str, ELEM" %f, %f, %f %f, %f, %f %f %f %d, %d, %d",
 			&s->cyl.origin.x, &s->cyl.origin.y, &s->cyl.origin.z,
 			&s->cyl.normal.x, &s->cyl.normal.y, &s->cyl.normal.z,

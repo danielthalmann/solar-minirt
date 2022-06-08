84bef4cecf9a13bb5696cb8653ccac240b4a97cf
Tanguy Rossel <trossel@student.42lausanne.ch>
2022-06-08 12:25:16 +0200

    Export PPM: Print prgress percentage to STDOUT

--- srcs/export_ppm.c
+++ srcs/export_ppm.c
@@ -6,7 +6,7 @@
 /*   By: trossel <trossel@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 19:09:27 by trossel           #+#    #+#             */
-/*   Updated: 2022/05/30 08:33:06 by trossel          ###   ########.fr       */
+/*   Updated: 2022/06/08 12:23:49 by trossel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,9 @@ int	export_ppm(char	*filename, t_app *app)
 	int				i;
 	int				color;
 	unsigned char	*ptr;
+	int				n_pixels;
 
+	n_pixels = app->scene.w * app->scene.h;
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	ptr = (unsigned char *)&color;
 	if (fd < 0)
@@ -31,16 +33,15 @@ int	export_ppm(char	*filename, t_app *app)
 		ft_fprintf(STDERR_FILENO, "Error: Could not open PPM\n");
 		return (1);
 	}
-	ft_fprintf(STDERR_FILENO, "Info: starting export to: %s\n", filename);
 	ft_fprintf(fd, "P3\n%d %d\n255\n", app->scene.w, app->scene.h);
 	i = -1;
 	while (++i < app->scene.w * app->scene.h)
 	{
 		color = app->pix_ptr[i];
 		ft_fprintf(fd, "%d %d %d\n", ptr[2], ptr[1], ptr[0]);
+		ft_printf("\rExport to PPM: %d %%", i * 100 / n_pixels);
 	}
+	ft_printf("\rExport to PPM: 100%%\n");
 	close(fd);
-	ft_fprintf(STDERR_FILENO, "Info: exported image to PPM file: %s\n",
-		filename);
 	return (0);
 }

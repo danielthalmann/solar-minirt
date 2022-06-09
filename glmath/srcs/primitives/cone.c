4630e155d3d6df64edf17802249f1c32fed398ba
Tanguy Rossel <tanguy.rossel@tyrossel.ch>
2022-06-09 15:10:44 +0200

    Norminette for cone

--- glmath/srcs/primitives/cone.c
+++ glmath/srcs/primitives/cone.c
@@ -6,7 +6,7 @@
 /*   By: trossel <trossel@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 14:12:35 by trossel           #+#    #+#             */
-/*   Updated: 2022/06/09 15:06:19 by trossel          ###   ########.fr       */
+/*   Updated: 2022/06/09 15:08:59 by trossel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,6 @@
 
 #include <math.h>
 #include <stdio.h>
-
-#define TS_PHI M_PI_4 / 2
-#define TS_RHO 0.5f
 
 void	cone_normal_ray(t_ray *normal, t_cone *cone)
 {
@@ -37,65 +34,22 @@ void	cone_normal_ray(t_ray *normal, t_cone *cone)
 	}
 }
 
-float	cone_color_mask(const t_ray *normale, const t_cone *cone)
-{
-	t_ray	r;
-	float	phi;
-	float	rho;
-	float	mask;
-
-	r = cylinder_world2cyl(normale, (t_cylinder *)cone);
-	rho = sqrt(r.origin.x * r.origin.x + r.origin.z * r.origin.z);
-	phi = atan2f(r.origin.z, r.origin.x);
-	mask = 1.0f;
-	if (r.origin.y >= cone->height || r.origin.y <= 0)
-	{
-		if ((fabsf(fmodf(phi + 1000, 2 * TS_PHI)) < TS_PHI
-		&& fabsf(fmodf(rho + 1000, 2 * TS_RHO)) > TS_RHO)
-		|| (fabsf(fmodf(phi + 1000, 2 * TS_PHI)) > TS_PHI
-		&& fmodf(rho, 2 * TS_RHO) < TS_RHO))
-			mask = 0.5f;
-	}
-	else if ((fabsf(fmodf(phi + 1000, 2 * TS_PHI)) < TS_PHI
-		&& fabsf(fmodf(r.origin.y + 1000, 2 * TS_RHO)) < TS_RHO)
-		|| (fabsf(fmodf(phi + 1000, 2 * TS_PHI)) > TS_PHI
-		&& fmodf(r.origin.y, 2 * TS_RHO) > TS_RHO))
-		mask = 0.5f;
-	return (mask);
-}
-
-t_color	cone_color_normal(const t_ray *normale, const t_cone *cone, float intensity)
-{
-	t_color	c;
-
-	(void) normale;
-	(void) cone;
-	(void) intensity;
-	c = color_create_int(0);
-	return (c);
-}
-
-t_color	cone_color_texture(const t_ray *normale, const t_cone *cone, t_image *texture)
-{
-	t_color	c;
-
-	(void) normale;
-	(void) cone;
-	(void) texture;
-	c = color_create_int(0);
-	return (c);
-}
-
 void	cone_print(const t_cone *cone)
 {
-	printf("\tCONE\n\torigin  = (%f, %f, %f)\n", cone->origin.x,
-		cone->origin.y, cone->origin.z);
-	printf("\tbase =      (%f, %f, %f)\n", cone->base[0].x, cone->base[1].x, cone->base[2].x);
-	printf("\t            (%f, %f, %f)\n", cone->base[0].y, cone->base[1].y, cone->base[2].y);
-	printf("\t            (%f, %f, %f)\n", cone->base[0].z, cone->base[1].z, cone->base[2].z);
-	printf("\tinv. base = (%f, %f, %f)\n", cone->base_inv[0].x, cone->base_inv[1].x, cone->base_inv[2].x);
-	printf("\t            (%f, %f, %f)\n", cone->base_inv[0].y, cone->base_inv[1].y, cone->base_inv[2].y);
-	printf("\t            (%f, %f, %f)\n", cone->base_inv[0].z, cone->base_inv[1].z, cone->base_inv[2].z);
+	printf("\tCONE\n\torigin  = (%f, %f, %f)\n",
+		cone->origin.x, cone->origin.y, cone->origin.z);
+	printf("\tbase =      (%f, %f, %f)\n",
+		cone->base[0].x, cone->base[1].x, cone->base[2].x);
+	printf("\t            (%f, %f, %f)\n",
+		cone->base[0].y, cone->base[1].y, cone->base[2].y);
+	printf("\t            (%f, %f, %f)\n",
+		cone->base[0].z, cone->base[1].z, cone->base[2].z);
+	printf("\tinv. base = (%f, %f, %f)\n",
+		cone->base_inv[0].x, cone->base_inv[1].x, cone->base_inv[2].x);
+	printf("\t            (%f, %f, %f)\n",
+		cone->base_inv[0].y, cone->base_inv[1].y, cone->base_inv[2].y);
+	printf("\t            (%f, %f, %f)\n",
+		cone->base_inv[0].z, cone->base_inv[1].z, cone->base_inv[2].z);
 	printf("\tradius =  %f\n", cone->radius);
 	printf("\theight =  %f\n", cone->height);
 }

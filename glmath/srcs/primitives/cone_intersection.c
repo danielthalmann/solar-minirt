69a84d43370f043037b344fa64a15e86978d5e31
Tanguy Rossel <trossel@student.42lausanne.ch>
2022-06-10 13:01:01 +0200

    Fix cone intersection

--- glmath/srcs/primitives/cone_intersection.c
+++ glmath/srcs/primitives/cone_intersection.c
@@ -6,7 +6,7 @@
 /*   By: dthalman <daniel@thalmann.li>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 10:26:45 by dthalman          #+#    #+#             */
-/*   Updated: 2022/06/09 14:36:30 by trossel          ###   ########.fr       */
+/*   Updated: 2022/06/10 12:50:52 by trossel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,46 +25,28 @@
 // 	return (0);
 // }
 
-// t_ends are the coefficients for the ray to reach the discs (actually planes)
-// at the ends of the cone. t_ends[0] is the smallest of them.
-//
-static void	init_t_ends(t_ray *r, const t_cone *cyl, float *t_ends)
-{
-	float	tmp;
-
-	t_ends[0] = -2.0f;
-	t_ends[1] = -1.0f;
-	if (fabsf(r->direction.y) > 1e-6)
-	{
-		t_ends[0] = (0.0f - r->origin.y) / r->direction.y;
-		t_ends[1] = (cyl->height - r->origin.y) / r->direction.y;
-	}
-	if (t_ends[1] < t_ends[0])
-	{
-		tmp = t_ends[1];
-		t_ends[1] = t_ends[0];
-		t_ends[0] = tmp;
-	}
-}
-
 // t_sides are the coefficients for the ray to reach the walls of the cone.
-static float	check_height(t_ray *r, const t_cone *cyl, float t_sides[2])
+static float	check_height(t_ray *r, const t_cone *c, float t_sides[2])
 {
-	t_v3f	inters[2];
-	float	t_ends[2];
+	t_v3f	inters[3];
+	float	t_end;
 	float	real_t;
+	float	rho;
 
 	real_t = -1.0f;
-	init_t_ends(r, cyl, t_ends);
+	t_end = -2.0f;
+	if (fabsf(r->direction.y) > 1e-6)
+		t_end = (c->height - r->origin.y) / r->direction.y;
 	inters[0] = ray_at(t_sides[0], r);
 	inters[1] = ray_at(t_sides[1], r);
-	if (t_sides[0] >= 0 && inters[0].y >= 0 && inters[0].y <= cyl->height)
+	inters[2] = ray_at(t_end, r);
+	rho = sqrtf(inters[2].x * inters[2].x + inters[2].z * inters[2].z);
+	if (t_sides[0] >= 0 && inters[0].y >= 0 && inters[0].y <= c->height)
 		real_t = t_sides[0];
-	else if (t_ends[0] >= 0 && t_ends[0] > t_sides[0] && t_ends[0] < t_sides[1])
-		real_t = t_ends[0];
-	else if (t_ends[1] >= 0 && t_ends[1] > t_sides[0] && t_ends[1] < t_sides[1])
-		real_t = t_ends[1];
-	else if (t_sides[1] >= 0 && inters[1].y >= 0 && inters[1].y <= cyl->height)
+	if (t_end >= 0 && rho < c->radius && (real_t < 0.0f || t_end < real_t))
+		real_t = t_end;
+	if (t_sides[1] >= 0 && (t_sides[1] < real_t || real_t < 0.0f)
+		&& inters[1].y >= 0 && inters[1].y <= c->height)
 		real_t = t_sides[1];
 	return (real_t);
 }

32227ee632cf61f8e8a9f67cc18bbeaacd6e60d2
Daniel Thalmann <daniel@thalmann.li>
2022-06-11 10:38:58 +0200

    init texture

--- srcs/texture.c
+++ srcs/texture.c
@@ -15,13 +15,14 @@
 #include "mlx.h"
 
 /**
- * @brief Charge en mémoire les textures
+ * @brief Charge en mémoire les textures et assigne les textures sur les objets
  * 
  * @param app 
  */
 void	init_texture(t_app *app)
 {
 	t_texture	*tex;
+	t_shape		*shape;
 
 	tex = app->scene.textures;
 	while (tex)
@@ -29,8 +30,25 @@ void	init_texture(t_app *app)
 		load_texture_xpm(tex->filename, app->mlx, &tex->image);
 		tex = tex->next;
 	}
+	shape = app->scene.shapes;
+	while (shape)
+	{
+		shape->texture[0] = get_texture_idx(app->scene.textures,
+				shape->tex_id[0]);
+		shape->texture[1] = get_texture_idx(app->scene.textures,
+				shape->tex_id[1]);
+		shape->normal_map = get_texture_idx(app->scene.textures, shape->nm_id);
+		shape = shape->next;
+	}
 }
 
+/**
+ * @brief retourne la texture en fonction de l'index de celle-ci
+ * 
+ * @param textures 
+ * @param index 
+ * @return t_texture* 
+ */
 t_texture	*get_texture_idx(t_texture *textures, int index)
 {
 	t_texture	*tex;
@@ -41,6 +59,11 @@ t_texture	*get_texture_idx(t_texture *textures, int index)
 	i = 0;
 	tex = textures;
 	while (tex && i < index)
+	{
 		tex = tex->next;
+		i++;
+	}
+	if (i != index)
+		return (NULL);
 	return (tex);
 }
